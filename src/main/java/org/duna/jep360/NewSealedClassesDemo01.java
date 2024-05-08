019aedc73eca36a60891609a6829376b0a8b8c1f
Eduardo Orrego <eduardo.orrego@outlook.com>
2024-05-08 00:36:27 -0500

    Java 15 Features - JEP 360: Sealed Classes (Preview)

--- src/main/java/org/duna/jep360/NewSealedClassesDemo01.java
+++ src/main/java/org/duna/jep360/NewSealedClassesDemo01.java
@@ -1,68 +1,69 @@
 package org.duna.jep360;
 
 public class NewSealedClassesDemo01 {
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-  }
+    }
 }
-
 abstract sealed class Shape permits Circle, Rectangle, Square {
-  // Define métodos y atributos comunes para todas las formas
-  public abstract double calculateArea();
+    // Define common methods and attributes for all forms.
+    public abstract double calculateArea();
 }
 
+//A permitted subclass may be declared final to prevent its part of the class hierarchy from being extended further.
 final class Circle extends Shape {
-  private final double radius;
+    private final double radius;
 
-  public Circle(double radius) {
-    this.radius = radius;
-  }
+    public Circle(double radius) {
+        this.radius = radius;
+    }
 
-  @Override
-  public double calculateArea() {
-    return Math.PI * radius * radius;
-  }
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
 }
 
+//A permitted subclass may be declared sealed to allow its part of the hierarchy to be extended further
+//than envisaged by its sealed superclass, but in a restricted fashion.
 sealed class Rectangle extends Shape permits TransparentRectangle, FilledRectangle {
-  private final double width;
-  private final double height;
+    private final double width;
+    private final double height;
 
-  public Rectangle(double width, double height) {
-    this.width = width;
-    this.height = height;
-  }
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
 
-  @Override
-  public double calculateArea() {
-    return width * height;
-  }
+    @Override
+    public double calculateArea() {
+        return width * height;
+    }
 }
 
 final class TransparentRectangle extends Rectangle {
-  public TransparentRectangle(double width, double height) {
-    super(width, height);
-  }
-  // Puedes agregar métodos específicos para TransparentRectangle
+    public TransparentRectangle(double width, double height) {
+        super(width, height);
+    }
 }
 
 final class FilledRectangle extends Rectangle {
-  public FilledRectangle(double width, double height) {
-    super(width, height);
-  }
-  // Puedes agregar métodos específicos para FilledRectangle
+    public FilledRectangle(double width, double height) {
+        super(width, height);
+    }
 }
 
+//A permitted subclass may be declared non-sealed so that its part of the hierarchy reverts to being open
+//for extension by unknown subclasses. (A sealed class cannot prevent its permitted subclasses from doing this.)
 non-sealed class Square extends Shape {
-  private final double side;
+    private final double side;
 
-  public Square(double side) {
-    this.side = side;
-  }
+    public Square(double side) {
+        this.side = side;
+    }
 
-  @Override
-  public double calculateArea() {
-    return side * side;
-  }
-  // Puedes agregar métodos específicos para Square
+    @Override
+    public double calculateArea() {
+        return side * side;
+    }
 }
